da2f2a88a1d74a973d7dcd037bf881ff29eb2f38
favour igbokwe <igbokwefc1@gmail.com>
2023-11-07 14:28:34 +0100

    undo

--- src/App.tsx
+++ src/App.tsx
@@ -23,14 +23,14 @@ const App = () => {
         {/* private route */}
         <Route element={<RootLayout/>}>
           <Route index element={<Home/>}/>
-          <Route path='/#explore' element={<Explore/>} />
-          <Route path='/#saved' element={<Saved/>} />
-          <Route path='/#all-users' element={<AllUsers/>} />
-          <Route path='/#create-post' element={<CreatePost/>} />
-          <Route path='/#update-post/:id' element={<EditPost/>} />
-          <Route path='/#posts/:id' element={<PostDetails/>} />
-          <Route path='/#profile/:id' element={<Profile/>} />
-          <Route path='/#update-profile/:id' element={<UpdateProfile/>} />
+          <Route path='/explore' element={<Explore/>} />
+          <Route path='/saved' element={<Saved/>} />
+          <Route path='/all-users' element={<AllUsers/>} />
+          <Route path='/create-post' element={<CreatePost/>} />
+          <Route path='/update-post/:id' element={<EditPost/>} />
+          <Route path='/posts/:id' element={<PostDetails/>} />
+          <Route path='/profile/:id' element={<Profile/>} />
+          <Route path='/update-profile/:id' element={<UpdateProfile/>} />
         </Route>
 
       </Routes>
